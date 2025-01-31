58d385cec3c0c981eb3ed27e05443c1d0a6bfb18
AndersBondeLudvigsen <144429290+AndersBondeLudvigsen@users.noreply.github.com>
2025-01-31 14:17:20 +0100

    made my homework

--- 3._Objects.js
+++ 3._Objects.js
@@ -5,19 +5,28 @@
 
 const myObject = { message: "Hello, earthling! I bring peace." };
 
+const message = myObject.message
+console.log(message)
+
 // Log the message 
 
 // --------------------------------------
 // Exercise 2 - Defining an object. 
 
 // Create an object that has your name and age. 
-
+const me ={
+    name: "Anders",
+    age: 23
+}
+console.log(me)
 
 // --------------------------------------
 // Exercise 3 - Add a key-value pair 
 
 const stackOverflow = {};
 
+stackOverflow.isAllowed = true
+console.log(stackOverflow)
 // make a rule called isAllowed and let the value be true
 
 // --------------------------------------
@@ -26,7 +35,9 @@ const stackOverflow = {};
 const thisSong = { description: "The best song in the world." };
 
 // remove the property "description" and add a property called "about" that should say "Just a tribute." 
-
+delete thisSong.description;
+thisSong.about = "The best song in the world"
+console.log(thisSong)
 
 // --------------------------------------
 
